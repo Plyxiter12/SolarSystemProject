7769f512db613383d5cbd7fec60430cef6cf5cd4
Andy <osilat3@gmail.com>
2014-02-24 17:47:04 -0500

    - created the gravitational constant - created the astronomical unit (for calculating distances) - created a sun object - created a planet class - planet properties are ratios of the sun's properties - created empty planet objects

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
 /**
  * Created by Andy Thompson on 2/19/14.
+ *
+ *
  */
+const G = 6.67e-11; //gravitational constant
+const AU = 149597871; //in km
+
+var sun = {
+    diameter: 1391000,
+    radius: (this.diameter/2),
+    mass: 1.989e30
+}
+
+/* parameters are ratios of the sun's dimensions */
+function Planet(diameter, mass, distance) {
+    this.diameter = (sun.diameter/diameter);
+    this.mass = (sun.mass/mass);
+    this.distance = (AU*distance);
+
+    var radius = (this.diameter/2);
+    var gravForce = function(Planet) {
+
+    }
+};
+
+var mercury = new Planet();
+var venus = new Planet();
+var earth = new Planet();
+var mars = new Planet();
+var jupiter = new Planet();
+var saturn = new Planet();
+var uranus = new Planet();
+var neptune = new Planet();
+var pluto = new Planet();
+
+var planetList = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto];
+
+var gravForce = function(planet1, planet2) {
+
+}
