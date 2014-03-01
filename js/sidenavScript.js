574a0f38e5c0620ddee458e872efed23e1ea7a3c
Justin Miller <superiorjt@live.com>
2014-03-01 01:34:58 -0500

    -added unordered list for planet infoText -gave planet infoText items common class ".infoText" -gave each planet infoText item a unique id -created "sidenavScript.js" which currently causes planet infoText items to appear or disappear onClick event

---
+++ js/sidenavScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('.planet-text').click(function () {
+        var planetName = $(this).children("a").text();
+        $('#planetInfo li p').each(function () {
+            if ($(this).is("#" + planetName.toLowerCase() + "Info") && $(this).css('opacity') == 1) {
+                $(this).animate({ opacity: 0 }, 1000);
+            }
+            else if ($(this).is("#" + planetName.toLowerCase() + "Info")) {
+                $(this).animate({ opacity: 1 }, 1000);
+            }
+            else {
+                $(this).animate({ opacity: 0 }, 1000);
+            }
+        });
+    });
+});
