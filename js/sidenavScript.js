053942b547de645bdf410540396370df68a190e2
SuperiorJT <jlmiller@bsugmail.net>
2014-03-05 16:39:50 -0500

    -merging to edit .infotext -added margin animation for .infotext

--- js/sidenavScript.js
+++ js/sidenavScript.js
@@ -2,14 +2,14 @@
     $('.planet-text').click(function () {
         var planetName = $(this).children("a").text();
         $('.infoText').each(function () {
-            if ($(this).is("#" + planetName.toLowerCase() + "Info") && $(this).css('opacity') === 1) {
-                $(this).animate({ opacity: 0 }, 1000);
+            if ($(this).is("#" + planetName.toLowerCase() + "Info") && $(this).css("opacity") == 1) {
+                $(this).animate({ opacity: 0, 'marginTop': +100 }, 500);
             }
             else if ($(this).is("#" + planetName.toLowerCase() + "Info")) {
-                $(this).animate({ opacity: 1 }, 1000);
+                $(this).animate({ opacity: 1, 'marginTop': -100 }, 500);
             }
             else {
-                $(this).animate({ opacity: 0 }, 1000);
+                $(this).animate({ opacity: 0, 'marginTop': +100 }, 500);
             }
         });
     });
